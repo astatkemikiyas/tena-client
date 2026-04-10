851faf52099e8696aac74c311f03665a7d098479
full name <mikiyas.astatke@gmail.com>
2026-04-10 15:36:55 +0200

    fix: fix specs and gitignore

--- src/app/api/model/models.ts
+++ src/app/api/model/models.ts
@@ -1,4 +1,4 @@
-not export * from './appointmentDTO';
+export * from './appointmentDTO';
 export * from './availabilitySlotDTO';
 export * from './hospitalInfoDTO';
 export * from './publicDoctorDTO';
